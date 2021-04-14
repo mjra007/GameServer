7cc4290c366b93e07f6876633fd997dfec203fb0
cabeca1143 <rafa2002002@gmail.com>
2021-04-14 18:21:52 -0300

    Partial implementation of Veigar's E
    Is spawns the cage Particle FX, but i haven't figured out the stun/stun hitbox yet

---
+++ Content/LeagueSandbox-Scripts/Champions/Veigar/E.cs
@@ -0,0 +1,103 @@
+using GameServerCore.Enums;
+using GameServerCore.Domain.GameObjects;
+using GameServerCore.Domain.GameObjects.Spell;
+using GameServerCore.Domain.GameObjects.Spell.Missile;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using System.Numerics;
+using LeagueSandbox.GameServer.API;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+
+namespace Spells
+{
+    public class VeigarEventHorizon : ISpellScript
+    {
+        Vector2 truecoords;
+        int ticks;
+        IObjAiBase Owner;
+        ISpell SPELL;
+        UnitCrowdControl stun;
+        float duration;
+
+        public ISpellScriptMetadata ScriptMetadata => new SpellScriptMetadata()
+        {
+            TriggersSpellCasts = true,
+            IsDamagingSpell = true,
+
+        };
+
+        public void OnActivate(IObjAiBase owner, ISpell spell)
+        {
+        }
+
+        public void TargetExecute(ISpell spell, IAttackableUnit target, ISpellMissile missile)
+        {
+        }
+
+        public void OnDeactivate(IObjAiBase owner, ISpell spell)
+        {
+        }
+
+        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
+        {
+
+        }
+
+        public void OnSpellCast(ISpell spell)
+        {
+            //ticks = 0;
+            //SPELL = spell;
+            truecoords = new Vector2(spell.CastInfo.TargetPosition.X, spell.CastInfo.TargetPosition.Z);
+            var distance = Vector2.Distance(spell.CastInfo.Owner.Position, truecoords);
+            if (distance > 650f)
+            {
+                truecoords = GetPointFromUnit(spell.CastInfo.Owner, 650f);
+            }
+
+            AddParticle(spell.CastInfo.Owner, "Veigar_Base_E_cas.troy", truecoords);
+            AddParticle(spell.CastInfo.Owner, "Veigar_Base_E_cage_green.troy", truecoords, lifetime: 3f);
+
+
+
+            //duration = 1.25f + (0.25f * spell.CastInfo.SpellLevel);
+            //stun = new UnitCrowdControl(CrowdControlType.STUN, duration);
+
+
+            //TODO: Make stun work Properly
+        }
+
+        public void OnSpellPostCast(ISpell spell)
+        {
+        }
+
+        public void OnSpellChannel(ISpell spell)
+        {
+        }
+
+        public void OnSpellChannelCancel(ISpell spell)
+        {
+        }
+
+        public void OnSpellPostChannel(ISpell spell)
+        {
+        }
+
+        public void OnUpdate(float diff)
+        {
+            //ticks++;
+
+            //if (ticks <= 180)
+            //{
+            //    var units = GetUnitsInRange(truecoords, 350f, true);
+            //    for (int i = 0; i < units.Count; i++)
+            //    {
+            //        if (Vector2.Distance(units[i].Position, truecoords) >= 350f && Vector2.Distance(units[i].Position, truecoords) <= 370f)
+            //        {
+            //            units[i].ApplyCrowdControl(stun, Owner);
+            //            AddBuff("VeigarEventHorizon", duration, 1, SPELL, units[i], Owner);
+
+            //        }
+            //    }
+            
+        }
+    }
+}
