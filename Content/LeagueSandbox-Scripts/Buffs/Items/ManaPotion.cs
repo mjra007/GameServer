86d8cb9910f5a7b2178fcc499acdc519c8924b15
cabeca1143 <rafa2002002@gmail.com>
2021-04-15 21:20:29 -0300

    Added Mana Potion spell and Buff scripts
    Fully functional

---
+++ Content/LeagueSandbox-Scripts/Buffs/Items/ManaPotion.cs
@@ -0,0 +1,38 @@
+﻿using GameServerCore.Domain.GameObjects;
+using GameServerCore.Enums;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using LeagueSandbox.GameServer.GameObjects.Stats;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using GameServerCore.Domain.GameObjects.Spell;
+
+namespace FlaskOfCrystalWater
+{
+    internal class FlaskOfCrystalWater : IBuffGameScript
+    {
+        public BuffType BuffType => BuffType.HEAL;
+        public BuffAddType BuffAddType => BuffAddType.STACKS_AND_CONTINUE;
+        public int MaxStacks => 25;
+        public bool IsHidden => false;
+
+        public IStatsModifier StatsModifier { get; private set; } = new StatsModifier();
+
+        IParticle potion;
+
+        public void OnActivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+            StatsModifier.ManaRegeneration.FlatBonus = 6.67f;
+            unit.AddStatModifier(StatsModifier);
+            potion = AddParticleTarget(ownerSpell.CastInfo.Owner, "GLOBAL_Item_ManaPotion.troy", unit, 1, lifetime: buff.Duration);
+        }
+
+        public void OnDeactivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+            potion.SetToRemove();
+        }
+
+        public void OnUpdate(float diff)
+        {
+
+        }
+    }
+}
