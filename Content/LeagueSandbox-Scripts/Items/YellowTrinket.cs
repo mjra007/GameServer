97ffa7b8e038d6dc1acd4eb39b722b7686fdb5ab
cabeca1143 <rafa2002002@gmail.com>
2021-04-15 21:34:55 -0300

    Added early Yellow trinket support

---
+++ Content/LeagueSandbox-Scripts/Items/YellowTrinket.cs
@@ -0,0 +1,73 @@
+﻿using GameServerCore.Domain.GameObjects;
+using GameServerCore.Domain.GameObjects.Spell;
+using GameServerCore.Domain.GameObjects.Spell.Missile;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using System.Numerics;
+
+namespace Spells
+{
+    public class TrinketTotemLvl1 : ISpellScript
+    {
+        public ISpellScriptMetadata ScriptMetadata => new SpellScriptMetadata()
+        {
+            // TODO
+        };
+
+        public void OnActivate(IObjAiBase owner, ISpell spell)
+        {
+        }
+
+        public void OnDeactivate(IObjAiBase owner, ISpell spell)
+        {
+        }
+
+        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
+        {
+            var Cursor = new Vector2(spell.CastInfo.TargetPosition.X, spell.CastInfo.TargetPosition.Z);
+            var current = new Vector2(owner.Position.X, owner.Position.Y);
+            var distance = Cursor - current;
+            Vector2 truecoords;
+            if (distance.Length() > 500f)
+            {
+                distance = Vector2.Normalize(distance);
+                var range = distance * 500f;
+                truecoords = current + range;
+            }
+            else
+            {
+                truecoords = Cursor;
+            }
+
+            var minion = AddMinion(owner, "YellowTrinket", "YellowTrinket", truecoords);
+            CreateTimer(60f, () =>
+            {
+                minion.Die(minion);
+            });
+        }
+
+        public void OnSpellCast(ISpell spell)
+        {
+        }
+
+        public void OnSpellPostCast(ISpell spell)
+        {
+        }
+
+        public void OnSpellChannel(ISpell spell)
+        {
+        }
+
+        public void OnSpellChannelCancel(ISpell spell)
+        {
+        }
+
+        public void OnSpellPostChannel(ISpell spell)
+        {
+        }
+
+        public void OnUpdate(float diff)
+        {
+        }
+    }
+}
