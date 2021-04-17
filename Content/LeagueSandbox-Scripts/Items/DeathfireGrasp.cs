32ca76727c89e0f0ad06d719dfb60b0fa06ff2f6
cabeca1143 <rafa2002002@gmail.com>
2021-04-16 23:53:50 -0300

    Yorick's Q, W, E Implemented
    Very basic AI and still have to work on the E ghoul AA lifesteal

--- Content/LeagueSandbox-Scripts/Items/DeathfireGrasp.cs
+++ Content/LeagueSandbox-Scripts/Items/DeathfireGrasp.cs
@@ -5,14 +5,22 @@ using GameServerCore.Domain.GameObjects.Spell;
 using GameServerCore.Domain.GameObjects.Spell.Missile;
 using GameServerCore.Enums;
 using System.Numerics;
+using LeagueSandbox.GameServer.API;
+using System.Collections.Generic;
 
 namespace Spells
 {
-    public class DeathfireGrasp : ISpellScript
+    public class DeathfireGraspSpell : ISpellScript
     {
         public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
         {
-            // TODO
+            TriggersSpellCasts = true,
+            IsDamagingSpell = true,
+            MissileParameters = new MissileParameters
+            {
+                Type = MissileType.Target
+            },
+
         };
 
         public void OnActivate(IObjAiBase owner, ISpell spell)
@@ -25,8 +33,78 @@ namespace Spells
 
         public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
         {
-            //spell.AddProjectileTarget("DeathfireGraspSpell", spell.CastInfo.SpellCastLaunchPosition, target);
-            AddBuff("Deathfire Grasp", 4.0f, 1, spell, target, owner);
+        }
+
+        public void OnSpellCast(ISpell spell)
+        {
+
+        }
+
+        public void OnSpellPostCast(ISpell spell)
+        {
+        }
+
+        public void OnSpellChannel(ISpell spell)
+        {
+        }
+
+        public void OnSpellChannelCancel(ISpell spell)
+        {
+        }
+
+        public void OnSpellPostChannel(ISpell spell)
+        {
+        }
+
+        public void OnUpdate(float diff)
+        {
+        }
+    }
+
+    public class DeathfireGrasp : ISpellScript
+    {
+        public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        {
+            MissileParameters = new MissileParameters
+            {
+                Type = MissileType.Target
+            },
+            IsDamagingSpell = true,
+            TriggersSpellCasts = true
+
+        };
+
+
+        public void OnActivate(IObjAiBase owner, ISpell spell)
+        {
+            ApiEventManager.OnSpellHit.AddListener(this, new KeyValuePair<ISpell, IObjAiBase>(spell, owner), TargetExecute, false);
+        }
+
+        public void OnDeactivate(IObjAiBase owner, ISpell spell)
+        {
+        }
+
+        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
+        {
+        }
+
+        public void TargetExecute(ISpell spell, IAttackableUnit target, ISpellMissile missile)
+        {
+            var owner = spell.CastInfo.Owner;
+
+            var damage = target.Stats.HealthPoints.Total * 0.15f;
+            
+
+            AddParticleTarget(owner, "deathFireGrasp_tar.troy", target, lifetime: 1f);
+
+            target.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
+
+            if (target.IsDead)
+            {
+                AddParticle(owner, "deathFireGrasp_tar.troy", target.Position, lifetime: 1f);
+            }
+            missile.SetToRemove();
+
         }
 
         public void OnSpellCast(ISpell spell)
@@ -35,13 +113,6 @@ namespace Spells
 
         public void OnSpellPostCast(ISpell spell)
         {
-        }
-
-        public void ApplyEffects(IObjAiBase owner, IAttackableUnit target, ISpell spell, ISpellMissile missile)
-        {
-            var damage = target.Stats.HealthPoints.Total * 0.15f;
-            target.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_DEFAULT, false);
-            missile.SetToRemove();
         }
 
         public void OnSpellChannel(ISpell spell)
