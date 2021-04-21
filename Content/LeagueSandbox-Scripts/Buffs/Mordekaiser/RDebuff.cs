8eb1aafce936a379ca456d97fbf44180fba36fa0
cabeca1143 <rafa2002002@gmail.com>
2021-04-20 22:25:32 -0300

    Implemented Mordekaiser's Q, W and R

---
+++ Content/LeagueSandbox-Scripts/Buffs/Mordekaiser/RDebuff.cs
@@ -0,0 +1,64 @@
+using GameServerCore.Domain.GameObjects;
+using GameServerCore.Domain.GameObjects.Spell;
+using GameServerCore.Enums;
+using LeagueSandbox.GameServer.GameObjects.Stats;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using System.Numerics;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+
+
+namespace MordekaiserChildrenOfTheGrave
+{
+    internal class MordekaiserChildrenOfTheGrave : IBuffGameScript
+    {
+        IAttackableUnit Unit;
+        float ticks;
+        float damage2;
+        IObjAiBase Owner;
+        IParticle p;
+
+        public BuffType BuffType => BuffType.SLOW;
+        public BuffAddType BuffAddType => BuffAddType.STACKS_AND_RENEWS;
+        public int MaxStacks => 1;
+        public bool IsHidden => false;
+
+        public IStatsModifier StatsModifier { get; private set; } = new StatsModifier();
+
+        public void OnActivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+            Unit = unit;
+            var owner = ownerSpell.CastInfo.Owner;
+
+            var damage = unit.Stats.HealthPoints.Total * (0.12f + (0.025f * (ownerSpell.CastInfo.SpellLevel - 1)) + (owner.Stats.AbilityPower.Total * 0.0002f));
+
+            unit.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
+            owner.Stats.CurrentHealth += damage;
+
+            p = AddParticleTarget(owner, "mordekeiser_cotg_tar.troy", unit, lifetime: buff.Duration); 
+
+
+            Owner = owner;
+            damage2 = unit.Stats.HealthPoints.Total * (0.012f + (0.0025f * (ownerSpell.CastInfo.SpellLevel - 1)) + (owner.Stats.AbilityPower.Total * 0.00002f));
+        }
+
+        public void OnDeactivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+            RemoveParticle(p);
+        }
+
+        public void OnUpdate(float diff)
+        {
+            ticks++;
+
+            if(ticks == 60f) 
+            {
+                Unit.TakeDamage(Unit, damage2, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_PERIODIC, false);
+                Owner.Stats.CurrentHealth = Owner.Stats.CurrentHealth + damage2;
+                ticks = 0;
+            }
+
+        }
+    }
+}
+//TODO: Make healing for POST MITIGATION damage
---
+++ Content/LeagueSandbox-Scripts/Champions/Mordekaiser/R.cs
@@ -0,0 +1,101 @@
+﻿using GameServerCore.Enums;
+using GameServerCore.Domain.GameObjects;
+using GameServerCore.Domain.GameObjects.Spell;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using System.Numerics;
+using LeagueSandbox.GameServer.API;
+using System.Collections.Generic;
+using GameServerCore.Domain.GameObjects.Spell.Missile;
+
+
+namespace Spells
+{
+    public class MordekaiserChildrenOfTheGrave : ISpellScript
+    {
+        IMinion minion;
+        IObjAiBase Owner;
+        IBuff Buff;
+        ISpell Spell;
+        IAttackableUnit Target;
+        public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        {
+            TriggersSpellCasts = true,
+            NotSingleTargetSpell = true
+            // TODO
+        };
+
+        public void OnActivate(IObjAiBase owner, ISpell spell)
+        {
+        }
+
+        public void OnDeactivate(IObjAiBase owner, ISpell spell)
+        {
+        }
+
+        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
+        {
+            Owner = owner;
+            Target = target;
+        }
+
+        public void OnSpellCast(ISpell spell)
+        {
+            Buff = AddBuff("MordekaiserChildrenOfTheGrave", 10.0f, 1, spell, Target, Owner);
+        }
+
+        public void OnSpellPostCast(ISpell spell)
+        {
+        }
+
+        public void OnSpellChannel(ISpell spell)
+        {
+        }
+
+        public void OnSpellChannelCancel(ISpell spell)
+        {
+        }
+
+        public void OnSpellPostChannel(ISpell spell)
+        {
+        }
+        public void TargetExecute(IAttackableUnit unit, bool isCrit)
+        {
+           /* if (Owner.HasBuff("MordekaiserMaceOfSpades"))
+            {
+                var ADratio = Owner.Stats.AttackDamage.FlatBonus;
+                var APratio = Owner.Stats.AbilityPower.Total * 0.4f;
+                var damage = 80f + (30 * (Spell.CastInfo.SpellLevel - 1)) + ADratio + APratio;
+
+                var units = GetUnitsInRange(Owner.Position, 600f, true);
+                for (int i = 0; i < units.Count; i++)
+                {
+                    if (units[i].Team != Owner.Team && !(units[i] is IObjBuilding || units[i] is IBaseTurret) && units[i] is IObjAiBase ai)
+                    {
+                        if ((units.Count - 1) == 1)
+                        {
+                            units[i].TakeDamage(Owner, damage * 1.65f, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_ATTACK, true);
+                        }
+                        else
+                        {
+                            units[i].TakeDamage(Owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_ATTACK, false);
+                        }
+                    }
+                }
+                RemoveBuff(Buff);
+
+
+                //Ghould debuff (Sets All the stats and decaying health)
+
+                                //MovSpeed Buffs:
+
+            }*/
+        }
+        public void OnUpdate(float diff)
+        {
+
+        }
+
+
+    }
+}
