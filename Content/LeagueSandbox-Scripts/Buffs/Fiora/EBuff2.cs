a3c6022436d889e2f5f34354f5891bd99deac229
cabeca1143 <rafa2002002@gmail.com>
2021-04-25 21:42:37 -0300

    Fiora's E

---
+++ Content/LeagueSandbox-Scripts/Buffs/Fiora/EBuff2.cs
@@ -0,0 +1,46 @@
+﻿using System.Numerics;
+using GameServerCore.Domain.GameObjects;
+using GameServerCore.Domain.GameObjects.Spell;
+using GameServerCore.Enums;
+using LeagueSandbox.GameServer.API;
+using LeagueSandbox.GameServer.GameObjects.Stats;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+
+namespace FioraFlurryDummy
+{
+    class FioraFlurryDummy : IBuffGameScript
+    {
+        public BuffType BuffType => BuffType.COMBAT_ENCHANCER;
+        public BuffAddType BuffAddType => BuffAddType.STACKS_AND_RENEWS;
+        public int MaxStacks => 3;
+        public bool IsHidden => false;
+
+        public IStatsModifier StatsModifier { get; private set; } = new StatsModifier();
+
+        IParticle pbuff;
+        IParticle pbuff2;
+        IBuff thisBuff;
+
+        public void OnActivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+            StatsModifier.MoveSpeed.PercentBonus = 0.07f + (0.02f * (ownerSpell.CastInfo.SpellLevel - 1));
+            unit.AddStatModifier(StatsModifier);
+        }
+
+        public void OnDeactivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+
+
+        }
+
+        public void OnPreAttack(ISpell spell)
+        {
+
+        }
+
+        public void OnUpdate(float diff)
+        {
+        }
+    }
+}
