9caed33548c6e127879dfec62af341d3ed642bb5
cabeca1143 <rafa2002002@gmail.com>
2021-04-29 15:31:40 -0300

    Akali's E
    Optimized the code and removed the cooldown lowering since it wasn't stated on her wiki

--- Content/LeagueSandbox-Scripts/Champions/Akali/E.cs
+++ Content/LeagueSandbox-Scripts/Champions/Akali/E.cs
@@ -7,6 +7,8 @@ using GameServerCore.Enums;
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
 using LeagueSandbox.GameServer.Scripting.CSharp;
 using System.Numerics;
+using LeagueSandbox.GameServer.API;
+using System.Collections.Generic;
 
 namespace Spells
 {
@@ -28,6 +30,7 @@ namespace Spells
 
         public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
         {
+
         }
 
         public void OnSpellCast(ISpell spell)
@@ -36,18 +39,16 @@ namespace Spells
 
         public void OnSpellPostCast(ISpell spell)
         {
+            var owner = spell.CastInfo.Owner;
             var ap = spell.CastInfo.Owner.Stats.AbilityPower.Total * 0.3f;
             var ad = spell.CastInfo.Owner.Stats.AttackDamage.Total * 0.6f;
             var damage = 40 + spell.CastInfo.SpellLevel * 30 + ap + ad;
-            foreach (var enemyTarget in GetUnitsInRange(spell.CastInfo.Owner.Position, 300, true)
-                .Where(x => x.Team == CustomConvert.GetEnemyTeam(spell.CastInfo.Owner.Team)))
+
+            var units = GetUnitsInRange(owner.Position, 300f, true);
+            for (int i = 0; i < units.Count; i++)
             {
-                enemyTarget.TakeDamage(spell.CastInfo.Owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL,
-                    false);
-                if (spell.CastInfo.Targets[0].Unit.IsDead)
-                {
-                    spell.LowerCooldown(spell.CurrentCooldown * 0.6f);
-                }
+                units[i].TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELLAOE, false);
+
             }
         }
 
