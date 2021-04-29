f60eb2cfd57f07224b2d606686077b98b30291f4
cabeca1143 <rafa2002002@gmail.com>
2021-04-29 15:23:22 -0300

    Fixed Blitzcrank's Q

--- Content/LeagueSandbox-Scripts/Champions/Blitzcrank/Q.cs
+++ Content/LeagueSandbox-Scripts/Champions/Blitzcrank/Q.cs
@@ -5,6 +5,8 @@ using GameServerCore.Domain.GameObjects.Spell;
 using GameServerCore.Domain.GameObjects.Spell.Missile;
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
 using LeagueSandbox.GameServer.Scripting.CSharp;
+using LeagueSandbox.GameServer.API;
+using System.Collections.Generic;
 
 namespace Spells
 {
@@ -24,27 +26,74 @@ namespace Spells
         {
         }
 
-        public void OnSpellCast(ISpell spell)
+        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
         {
             //spell.CastInfo.Owner.SpellAnimation("SPELL1");
+
+
+        }
+
+        public void OnSpellCast(ISpell spell)
+        {
+            var current = new Vector2(spell.CastInfo.Owner.Position.X, spell.CastInfo.Owner.Position.Y);
+            var MousePos = new Vector2(spell.CastInfo.TargetPosition.X, spell.CastInfo.TargetPosition.Z);
+            var to = Vector2.Normalize(MousePos - current);
+            var range = to * 925;
+            var trueCoords = current + range;
+
+
+            SpellCast(owner, 0, SpellSlotType.ExtraSlots, trueCoords, trueCoords, false, Vector2.Zero);
+            PlayAnimation(spell.CastInfo.Owner, "SPELL1");
+        }
+
+        public void OnSpellPostCast(ISpell spell)
+        {
+        }
+        public void OnSpellChannel(ISpell spell)
+        {
+        }
+
+        public void OnSpellChannelCancel(ISpell spell)
+        {
+        }
+
+        public void OnSpellPostChannel(ISpell spell)
+        {
+        }
+
+        public void OnUpdate(float diff)
+        {
+
+        }
+    }
+    public class RocketGrabMissile : ISpellScript
+    {
+        public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        {
+            MissileParameters = new MissileParameters
+            {
+                Type = MissileType.Circle
+            },
+            IsDamagingSpell = true
+            // TODO
+        };
+
+        public void OnActivate(IObjAiBase owner, ISpell spell)
+        {
+            ApiEventManager.OnSpellHit.AddListener(this, new KeyValuePair<ISpell, IObjAiBase>(spell, owner), TargetExecute, false);
+        }
+
+        public void OnDeactivate(IObjAiBase owner, ISpell spell)
+        {
         }
 
         public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
         {
         }
 
-        public void OnSpellPostCast(ISpell spell)
+        public void TargetExecute(ISpell spell, IAttackableUnit target, ISpellMissile missile)
         {
-            var current = new Vector2(spell.CastInfo.Owner.Position.X, spell.CastInfo.Owner.Position.Y);
-            var spellPos = new Vector2(spell.CastInfo.TargetPosition.X, spell.CastInfo.TargetPosition.Z);
-            var to = Vector2.Normalize(spellPos - current);
-            var range = to * 925;
-            var trueCoords = current + range;
-            //spell.AddProjectile("RocketGrabMissile", current, current, trueCoords);
-        }
-
-        public void ApplyEffects(IObjAiBase owner, IAttackableUnit target, ISpell spell, ISpellMissile missile)
-        {
+            var owner = spell.CastInfo.Owner;
             var ap = owner.Stats.AbilityPower.Total;
             var damage = 25 + spell.CastInfo.SpellLevel * 55 + ap;
             target.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
@@ -60,6 +109,15 @@ namespace Spells
             }
 
             missile.SetToRemove();
+
+        }
+
+        public void OnSpellCast(ISpell spell)
+        {
+        }
+
+        public void OnSpellPostCast(ISpell spell)
+        {
         }
 
         public void OnSpellChannel(ISpell spell)
