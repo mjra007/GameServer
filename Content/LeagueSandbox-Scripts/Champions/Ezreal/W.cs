a30005089c323316c276cad6740dc9439955565e
cabeca1143 <rafa2002002@gmail.com>
2021-04-29 15:24:43 -0300

    Fixed Ezreal's W and R
    Still have to fix damage reduction on R tho

--- Content/LeagueSandbox-Scripts/Champions/Ezreal/W.cs
+++ Content/LeagueSandbox-Scripts/Champions/Ezreal/W.cs
@@ -5,6 +5,8 @@ using GameServerCore.Enums;
 using LeagueSandbox.GameServer.Scripting.CSharp;
 using GameServerCore.Domain.GameObjects.Spell;
 using GameServerCore.Domain.GameObjects.Spell.Missile;
+using LeagueSandbox.GameServer.API;
+using System.Collections.Generic;
 
 namespace Spells
 {
@@ -35,36 +37,97 @@ namespace Spells
 
         public void OnSpellPostCast(ISpell spell)
         {
-            var current = new Vector2(spell.CastInfo.Owner.Position.X, spell.CastInfo.Owner.Position.Y);
+            var owner = spell.CastInfo.Owner as IChampion;
+            var current = new Vector2(owner.Position.X, owner.Position.Y);
             var spellPos = new Vector2(spell.CastInfo.TargetPosition.X, spell.CastInfo.TargetPosition.Z);
             var to = Vector2.Normalize(spellPos - current);
             var range = to * 1000;
             var trueCoords = current + range;
+            SpellCast(owner, 2, SpellSlotType.ExtraSlots, trueCoords, trueCoords, false, Vector2.Zero);
             //spell.AddProjectile("EzrealEssenceFluxMissile", new Vector2(spell.CastInfo.SpellCastLaunchPosition.X, spell.CastInfo.SpellCastLaunchPosition.Z), trueCoords, trueCoords, overrideCastPosition: true);
         }
 
         public void ApplyEffects(IObjAiBase owner, IAttackableUnit target, ISpell spell, ISpellMissile missile)
         {
-            var champion = target as IChampion;
 
+        }
+
+        public void OnSpellChannel(ISpell spell)
+        {
+        }
+
+        public void OnSpellChannelCancel(ISpell spell)
+        {
+        }
+
+        public void OnSpellPostChannel(ISpell spell)
+        {
+        }
+
+        public void OnUpdate(float diff)
+        {
+        }
+    }
+    public class EzrealEssenceFluxMissile : ISpellScript
+    {
+        public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        {
+            MissileParameters = new MissileParameters
+            {
+                Type = MissileType.Circle
+            },
+            IsDamagingSpell = true
+            // TODO
+        };
+
+        //Vector2 direction;
+
+        public void OnActivate(IObjAiBase owner, ISpell spell)
+        {
+            ApiEventManager.OnSpellHit.AddListener(this, new KeyValuePair<ISpell, IObjAiBase>(spell, owner), TargetExecute, false);
+        }
+
+        public void OnDeactivate(IObjAiBase owner, ISpell spell)
+        {
+        }
+
+        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
+        {
+        }
+
+        public void TargetExecute(ISpell spell, IAttackableUnit target, ISpellMissile missile)
+        {
+            var champion = target as IChampion;
+            var owner = spell.CastInfo.Owner as IChampion;
             if (champion == null)
             {
                 return;
             }
-
-            var buffTime = 5f;
+            
             var ownerAbilityPowerTotal = owner.Stats.AbilityPower.Total;
 
-            if (champion.Team.Equals(owner.Team) && !champion.Equals(owner))
+            if (champion.Team == owner.Team || champion == owner) //fix not being able to self proc
             {
-                AddBuff("EzrealWBuff", buffTime, 1, spell, champion, owner);
+                AddBuff("EzrealWBuff", 5f, 1, spell, champion, owner);
+                AddParticleTarget(owner, "Ezreal_essenceflux_tar.troy", champion);
             }
             else
             {
                 var damage = 25 + (45 * spell.CastInfo.SpellLevel) + (ownerAbilityPowerTotal * 0.8f);
 
                 target.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL, DamageResultType.RESULT_NORMAL);
+                AddParticleTarget(owner, "Ezreal_essenceflux_tar.troy", champion);
             }
+
+            // SpellBuffAdd EzrealRisingSpellForce
+        }
+
+        public void OnSpellCast(ISpell spell)
+        {
+        }
+
+        public void OnSpellPostCast(ISpell spell)
+        {
         }
 
         public void OnSpellChannel(ISpell spell)
